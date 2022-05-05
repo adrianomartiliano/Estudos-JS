1d129f0636fc7de1920595120305218e283b2df0
Adriano Martiliano <adrianomartiliano@gmail.com>
2022-05-05 17:40:59 -0300

    Calculadora

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 /*function guardar(){
     var nome = prompt("Qual o seu nome?")
     alert("O seu nome é "+ nome)
-}*/
+}
 function idade(){
     var anonasci = prompt("Informe seu ano de nascimento: ")
     while(anonasci >= 2022){
@@ -12,4 +12,33 @@ function idade(){
 
     var idadefinal = anoatual - anonasci;
     alert("Sua idade é "+idadefinal)
-}
+}*/
+function calc(){
+    var n1 = prompt("Informe o primeiro número");
+    var op = prompt("Infome o sinal da operação (+ - / *)");
+    while ((op != '+') && (op != '-') && (op != '/') && (op != '*')){
+        var op = prompt("Infome o sinal da operação (+ - / *)");
+    }
+    var n2 = prompt("Informe o segundo número");
+
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+
+
+    if (op == "+"){
+        var resultado = n1 + n2;
+        alert("A soma entre "+n1+" e "+n2+" = "+resultado);
+    }
+    if (op == "-"){
+        var resultado = n1 - n2;
+        alert("A subtração entre "+n1+" e "+n2+" = "+resultado);
+    }
+    if (op == "/"){
+        var resultado = n1 / n2;
+        alert("A divisão entre "+n1+" por "+n2+" = "+resultado);
+    }
+    if (op == "*"){
+        var resultado = n1 * n2;
+        alert("A multiplicaçao entre "+n1+" e "+n2+" = "+resultado);
+    }
+} 
