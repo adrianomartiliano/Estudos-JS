898db7ee708473ccbde15e85fd63d2688686a814
Adriano Martiliano <adrianomartiliano@gmail.com>
2022-05-05 13:05:12 -0300

    Cálculo de idade

--- js/js.js
+++ js/js.js
@@ -1,4 +1,15 @@
-function guardar(){
+/*function guardar(){
     var nome = prompt("Qual o seu nome?")
     alert("O seu nome é "+ nome)
+}*/
+function idade(){
+    var anonasci = prompt("Informe seu ano de nascimento: ")
+    while(anonasci >= 2022){
+        alert("Ano errado... informe corretamente")
+        var anonasci = prompt("Ano de nascimento: ")
+    }
+    var anoatual = prompt("Qual o ano atual ?")
+
+    var idadefinal = anoatual - anonasci;
+    alert("Sua idade é "+idadefinal)
 }
