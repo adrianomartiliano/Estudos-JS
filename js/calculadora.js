4b592659b584e257c230247c0a2a24361167d107
Adriano Martiliano <adrianomartiliano@gmail.com>
2022-09-14 22:08:18 -0300

    Testando função

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,3 +1,11 @@
-function teste(){
-    alert("Deu certo");
+var display = document.querySelector('#display');
+
+var numeros = document.querySelectorAll('.numeros');
+var numeroClicado;
+
+    if (numeros[9].addEventListener('click', mudaDisplay)){
+        numeroClicado = '0';
+    }else if(numeros[0].addEventListener('click', mudaDisplay)){ numeroClicado = '1';}
+function mudaDisplay(){
+    display.innerHTML = numeroClicado;
 }
